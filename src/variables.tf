2c8f7f73a7589f24c31fb45b3a46d2a35842a713
Thiago Cruz <thagocruz@gmail.com>
2022-09-09 16:06:34 -0300

    add variables

---
+++ src/variables.tf
@@ -0,0 +1,36 @@
+# Input Variables
+variable "location" {
+  type = string
+  description = "Azure Region when all these resources will be provisioned"
+  default = "Central US"
+}
+
+variable "resource_group_name"{
+  type = string
+  description = "This variable defines the Resource Group"
+  default = "terraform-aks"
+}
+
+variable "environment" {
+  type = string
+  description = "This variable defines the Environment"
+  default = "dev"
+}
+
+# AKS Input Variables
+variable "ssh_public_key" {
+  default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+  description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"
+}
+
+variable "windows_admin_username" {
+  type = string
+  default = "azureuser"
+  description = "This variable defines the Windows admin username k8s Worker nodes"  
+}
+
+variable "windows_admin_password" {
+  type = string
+  default = "P@ssw0rd1234"
+  description = "This variable defines the Windows admin password k8s Worker nodes"  
+}
