fb48d323a0d1b87c587e838b5d20ab3113f59dc9
Thiago Cruz <thagocruz@gmail.com>
2022-09-09 16:31:32 -0300

    add default_node_pool

--- src/aks-cluster.tf
+++ src/aks-cluster.tf
@@ -5,4 +5,28 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   resource_group_name = azurerm_resource_group.aks_rg.name
   kubernetes_version = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
+
+  default_node_pool {
+    name = "systempool"
+    vm_size = "Standard_DS2_v2"
+    orquestration_version = data.azurerm_kubernetes_service_versions.current.latest_version
+    availability_zones = [1, 2, 3]
+    enable_auto_scaling = true
+    max_count = 3
+    min_count = 1
+    os_disk_size_gb = 30
+    type = "VirtualMachineScaleSets"
+    node_labels = {
+      "nodepool-type" = "system"
+      "environment" = var.environment
+      "nodepoolos" = "linux"
+      "app" = "system-apps"
+    }
+    tags = {
+      "nodepool-type" = "system"
+      "environment" = var.environment
+      "nodepoolos" = "linux"
+      "app" = "system-apps"
+    }
+  }
 }
