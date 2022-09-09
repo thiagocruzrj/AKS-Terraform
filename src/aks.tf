5898973c82091c1b85c072865d36e8d5792cec02
Thiago Cruz <thagocruz@gmail.com>
2022-09-09 16:13:55 -0300

    add azuread_group

--- src/aks.tf
+++ src/aks.tf
@@ -14,3 +14,9 @@ resource "azurerm_log_analytics_workspace" "insights" {
   resource_group_name = azurerm_resource_group.aks_rg.name
   retention_in_days = 30
 }
+
+resource "azuread_group" "aks_administrators" {
+  display_name = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
+  description = "Azure AKS administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster"
+  security_enabled = true
+}
