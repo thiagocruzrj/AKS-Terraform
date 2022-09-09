be4589d75c268dc6a4e80917afb417739b19b3c9
Thiago Cruz <thagocruz@gmail.com>
2022-09-09 16:11:21 -0300

    add azurerm_log_analytics_workspace

--- src/aks.tf
+++ src/aks.tf
@@ -7,3 +7,10 @@ data "azurerm_kubernetes_service_versions" "current" {
   location = azurerm_resource_group.aks_rg.location
   include_preview = false
 }
+
+resource "azurerm_log_analytics_workspace" "insights" {
+  name = "${var.environment}-logs-${random_pet.aksrandom.id}"
+  location = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  retention_in_days = 30
+}
